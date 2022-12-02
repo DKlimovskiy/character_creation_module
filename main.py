87068eff2251d73d9eeabad7303d398d02818592
Дмитрий Климовский <barsik2142@yandex.ru>
2022-12-02 22:18:32 +0300

    add working version whithout classes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -10,7 +11,7 @@ def attack(char_name: str, char_class: str) -> str:
                 f'{5 + randint(5, 10)}')
     if char_class == 'healer':
         return (f'{char_name} нанёс противнику урон, равный '
-                f'{5 + randint(-3, -1)}')
+                f'{5 + randint(-3, -1)}')s
 
 
 def defence(char_name: str, char_class: str) -> str:
@@ -78,7 +79,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,6 +90,6 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-    
+
 
 main()
